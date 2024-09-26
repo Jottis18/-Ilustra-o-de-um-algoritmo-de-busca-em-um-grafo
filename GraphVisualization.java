d89e02e051afb86c80ea5b280c08248b7b885d03
Gabriel da Silveira Miranda <gabrielsmbr@gmail.com>
2024-09-26 15:21:36 -0300

    Altura e largura da janela agora estao em variaveis

--- GraphVisualization.java
+++ GraphVisualization.java
@@ -1,10 +1,15 @@
 import javax.swing.*;
 
+
 public class GraphVisualization {
+
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
+
     public static void showGraph(Graph graph) {
         JFrame frame = new JFrame("Visualização do Grafo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        frame.setSize(WIDTH, HEIGHT);
 
         GraphPanel graphPanel = new GraphPanel(graph);
         frame.add(graphPanel);
