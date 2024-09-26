7f66ea6048c84758f4edd5aa120000cb017a9275
Gabriel da Silveira Miranda <gabrielsmbr@gmail.com>
2024-09-26 17:14:28 -0300

    added new features

--- Graph.java
+++ Graph.java
@@ -1,8 +1,20 @@
 import java.util.*;
 
+// Classe para representar uma aresta com destino e peso
+class Edge {
+    int dest;
+    int weight;
+
+    // Construtor
+    public Edge(int dest, int weight) {
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
+
 public class Graph {
     private int vertices;
-    private LinkedList<Integer> adjList[];
+    private LinkedList<Edge> adjList[];
 
     // Construtor
     public Graph(int vertices) {
@@ -13,17 +25,34 @@ public class Graph {
         }
     }
 
+    public boolean edgeExists(int src, int dest) {
+        for (Edge edge : adjList[src]) {
+            if (edge.dest == dest) {
+                return true; // A aresta já existe
+            }
+        }
+        return false;
+    }
+
     // Adiciona uma aresta ao grafo
-    public void addEdge(int src, int dest) {
+    public boolean addEdge(int src, int dest, int weight) {
         if (src >= 0 && src < vertices && dest >= 0 && dest < vertices) {
-            adjList[src].add(dest);
-            adjList[dest].add(src); // Grafo não direcionado
+            for (Edge edge : adjList[src]) {
+                if (edge.dest == dest) {
+                    System.out.println("Erro: Aresta já existe entre " + src + " e " + dest);
+                    return false;
+                }
+            }
+            adjList[src].add(new Edge(dest, weight));
+            adjList[dest].add(new Edge(src, weight)); // Grafo não direcionado
+            return true;
         } else {
             System.out.println("Erro: vértices fora dos limites (" + src + ", " + dest + ")");
+            return false;
         }
     }
 
-    public LinkedList<Integer>[] getAdjList() {
+    public LinkedList<Edge>[] getAdjList() {
         return adjList;
     }
 
